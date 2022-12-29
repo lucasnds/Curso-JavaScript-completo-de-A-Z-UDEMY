313eeb91043ecd999e8ce0a35e5a98b62f64a8ac
Lucas <lucasdoismile16@gmail.com>
2022-12-28 23:22:32 -0300

    app.js do buscacep

--- BuscaCep/app.js
+++ BuscaCep/app.js
@@ -4,17 +4,17 @@ const logradouro = document.querySelector("#logradouro");
 const bairro = document.querySelector("#bairro");
 const localidade = document.querySelector("#localidade");
 const uf = document.querySelector("#uf");
-const btnBuscar =  document.querySelector("#botao_buscar");
-const btnLimpar =  document.querySelector("#botao_limpar");
-const btnSalvar =  document.querySelector("#botao_salvar"); 
-let endereco; 
+const btnBuscar = document.querySelector("#botao_buscar");
+const btnLimpar = document.querySelector("#botao_limpar");
+const btnSalvar = document.querySelector("#botao_salvar");
+let endereco;
 
 btnBuscar.addEventListener('click', (e) => {
-    e.preventDefault(); 
-    try{
-        //validaCEP();
-    }catch(erro){
-        //mensagemErro.innerHTML = erro.message;
+    e.preventDefault();
+    try {
+        validaCEP();
+    } catch (erro) {
+        mensagemErro.innerHTML = erro.message;
     }
 })
 
@@ -27,7 +27,40 @@ btnSalvar.addEventListener('click', () => {
     limparCampos();
 })
 
-function limpaCampos(){
+function preencheCampos(endereco) {
+    for (const campo in endereco) {
+        if (document.querySelector("#" + campo)) {
+            document.querySelector("#" + campo).value = endereço[campo];
+        }
+
+    }
+}
+
+function validaCEP() {
+    const regex = /^([0-9]{5})-([0-9]{3})$/;
+    const regex1 = /^\d{8}$/;
+
+    if (regex.test(cepbusca.value) || regex1.test(cepbusca.value)) {
+        buscaEndereço();
+    } else {
+        throw new Error('CEP inválido');
+    }
+}
+
+function buscaEndereço() {
+    fetch(`http://viacep.com.br/ws/${cepbusca.value}/json`)
+        .then((resposta) => {
+            return resposta.json();
+        })
+        .then((endereco) => {
+            preencheCampos(endereco);
+        })
+        .catch((erro) => {
+            console.error(erro);
+        })
+}
+
+function limpaCampos() {
     cepbusca.value = "";
     logradouro.value = "";
     bairro.value = "";
@@ -35,6 +68,3 @@ function limpaCampos(){
     uf.value = "";
     mensagemErro.innerHTML = "";
 }
-
-
-
